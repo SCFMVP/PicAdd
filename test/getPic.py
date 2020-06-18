f2227e8d860634cd1b51460b73590d59145655a9
later <1638414737@qq.com>
2020-06-18 12:28:04 +0800

    initial commit 读取图片已优化

---
+++ test/getPic.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+# 两种图片读取方式:绝对路径,相对路径
+# img = cv2.imread('E:\PycharmProjects\PicAdd\\test\key.jpg', 0)
+img = cv2.imread('Test.png')  # 尾部加0则显示黑白
+
+# 显示图像尺寸信息
+sp = img.shape
+print(sp)
+height = sp[0]
+width = sp[1]
+print('width: %d \n height: %d \n' % (width, height))
+
+# 显示图片
+cv2.namedWindow('Image', 0)
+cv2.resizeWindow('Image', width, height)
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
